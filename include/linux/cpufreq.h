0ea67ea522a8110dc7c36eb5929309478a22384f
Andre Saddler <dresadd09691@gmail.com>
2014-01-01 23:48:35 -0500

    cpufreq: Add OndemandPLUS Gov
    Signed-off-by: houst0nn <houstonn@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	include/linux/cpufreq.h
    
    Conflicts:
    	arch/arm/configs/d800_defconfig
    	arch/arm/configs/d801_defconfig
    	arch/arm/configs/d802_defconfig
    	arch/arm/configs/d803_defconfig
    	arch/arm/configs/ls980_defconfig
    	arch/arm/configs/vs980_defconfig

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -381,6 +381,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
+extern struct cpufreq_governor cpufreq_gov_ondemandplus;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandplus)
 #endif
 
 
