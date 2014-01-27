45e189296d1a2c37f7f3ed06dca9082a0e22742f
myfluxi <linflux@arcor.de>
2014-01-27 01:21:10 +0100

    arm: vfpmodule: Fix warning procfs vfp_bounce reporting failed
    Creation of procfs cpu/vfp_bounce fails because we're initialized too early. Fix
    this by creating it on rootfs_initcall as before the NEON patches.
    
    <6>[    0.130770] VFP support v0.3: implementor 51 architecture 64 part 6f varia
    nt 2 rev 0
    <4>[    0.130795] ------------[ cut here ]------------
    <4>[    0.130813] WARNING: at fs/proc/generic.c:323 __xlate_proc_name+0xac/0xcc(
    )
    <4>[    0.130822] name 'cpu/vfp_bounce'
    <4>[    0.130855] [<c010e26c>] (unwind_backtrace+0x0/0x144) from [<c0a20f58>] (d
    ump_stack+0x20/0x24)
    <4>[    0.130879] [<c0a20f58>] (dump_stack+0x20/0x24) from [<c019b670>] (warn_sl
    owpath_common+0x58/0x70)
    <4>[    0.130899] [<c019b670>] (warn_slowpath_common+0x58/0x70) from [<c019b704>
    ] (warn_slowpath_fmt+0x40/0x48)
    <4>[    0.130919] [<c019b704>] (warn_slowpath_fmt+0x40/0x48) from [<c02c2ad8>] (
    __xlate_proc_name+0xac/0xcc)
    <4>[    0.130938] [<c02c2ad8>] (__xlate_proc_name+0xac/0xcc) from [<c02c2b50>] (
    __proc_create+0x58/0x100)
    <4>[    0.130956] [<c02c2b50>] (__proc_create+0x58/0x100) from [<c02c2ed0>] (pro
    c_create_data+0x5c/0xc0)
    <4>[    0.130979] [<c02c2ed0>] (proc_create_data+0x5c/0xc0) from [<c0f03484>] (v
    fp_init+0x19c/0x200)
    <4>[    0.131000] [<c0f03484>] (vfp_init+0x19c/0x200) from [<c0f00c98>] (do_one_
    initcall+0x98/0x168)
    <4>[    0.131020] [<c0f00c98>] (do_one_initcall+0x98/0x168) from [<c0f00e60>] (k
    ernel_init+0xf8/0x1b4)
    <4>[    0.131043] [<c0f00e60>] (kernel_init+0xf8/0x1b4) from [<c01081a0>] (kerne
    l_thread_exit+0x0/0x8)
    <4>[    0.131076] ---[ end trace ea6d9a9b5e947151 ]---
    <3>[    0.131086] Failed to create procfs node for VFP bounce reporting
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	arch/arm/vfp/vfpmodule.c

--- arch/arm/vfp/vfpmodule.c
+++ arch/arm/vfp/vfpmodule.c
@@ -749,9 +749,6 @@ static int __init vfp_init(void)
 {
 	unsigned int vfpsid;
 	unsigned int cpu_arch = cpu_architecture();
-#ifdef CONFIG_PROC_FS
-	static struct proc_dir_entry *procfs_entry;
-#endif
 	if (cpu_arch >= CPU_ARCH_ARMv6)
 		on_each_cpu(vfp_enable, NULL, 1);
 
@@ -821,7 +818,14 @@ static int __init vfp_init(void)
 		}
 	}
 
+	return 0;
+}
+
+static int __init vfp_rootfs_init(void)
+{
 #ifdef CONFIG_PROC_FS
+	static struct proc_dir_entry *procfs_entry;
+
 	procfs_entry = create_proc_entry("cpu/vfp_bounce", S_IRUGO, NULL);
 
 	if (procfs_entry)
@@ -841,3 +845,4 @@ core_initcall(vfp_init);
 #else /* origin */
 late_initcall(vfp_init);
 #endif
+rootfs_initcall(vfp_rootfs_init);
