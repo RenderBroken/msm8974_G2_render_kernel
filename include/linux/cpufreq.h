cac21173829ed3512315d9a197478bab43c46f97
Pranav Vashi <neobuddy89@gmail.com>
2014-02-11 03:43:16 +0530

    drivers: cpufreq: Add HYPER governor
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -395,6 +395,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
+extern struct cpufreq_governor cpufreq_gov_HYPER;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_HYPER)
 #endif
 
 
