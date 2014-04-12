2980e1f4230bdf39f98acdb5807ad4ba33fe504f
dorimanx <yuri@bynet.co.il>
2014-04-13 02:00:40 +0300

    Merged Force Fast Charge Driver by @Faux123 and ME :)

--- include/linux/fastchg.h
+++ include/linux/fastchg.h
@@ -1,5 +1,8 @@
 /*
- * Author: Chad Froebel <chadfroebel@gmail.com>
+ * based on work from:
+ *	Chad Froebel <chadfroebel@gmail.com> &
+ *	Jean-Pierre Rasquin <yank555.lu@gmail.com>
+ * for backwards compatibility
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -16,8 +19,23 @@
 #define _LINUX_FASTCHG_H
 
 extern int force_fast_charge;
+extern int fast_charge_level;
+extern int fake_charge_ac;
 
-#define FAST_CHARGE_DISABLED 0	/* default */
-#define FAST_CHARGE_FORCE_AC 1
+#define FAST_CHARGE_DISABLED		0	/* default */
+#define FAST_CHARGE_FORCE_AC		1
+#define FAST_CHARGE_FORCE_CUSTOM_MA	2
+
+#define FAST_CHARGE_500		500
+#define FAST_CHARGE_900		900
+#define FAST_CHARGE_1200	1200
+#define FAST_CHARGE_1500	1500
+#define FAST_CHARGE_1800	1800
+#define FAST_CHARGE_2000	2000
+
+#define FAST_CHARGE_LEVELS	"500 900 1200 1500 1800 2000"
+
+#define FAKE_CHARGE_AC_DISABLE	0
+#define FAKE_CHARGE_AC_ENABLE	1
 
 #endif
