09328fcdd750f9bf48e7f493c3bb412fa8a0f0c7
Pranav Vashi <neobuddy89@gmail.com>
2014-05-20 14:28:14 +0530

    msm: Ammend use of interruptible threads
    In commits 9bdb55867a042611be0ec4efb47d8202bfeaae54 and adf59281d2c3fa68714e4acb8ef7078e1dd77c97,
    we changed wait events to use interruptible threads
    to reduce high load avg.
    
    This commit just ammends the use of interruptible threads
    to serve the actual purpose of original wait events.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1986,14 +1986,10 @@ static int __mdss_fb_display_thread(void *data)
 		pr_info("%s: set priority failed\n", __func__);
 
 	while (1) {
-		ret = wait_event_interruptible(mfd->commit_wait_q,
-				(atomic_read(&mfd->commits_pending) ||
-				 kthread_should_stop()));
-
-		if (ret) {
-			pr_info("%s: interrupted", __func__);
-			continue;
-		}
+		while (wait_event_interruptible(
+			mfd->commit_wait_q,
+			(atomic_read(&mfd->commits_pending) ||
+			kthread_should_stop())) != 0);
 
 		if (kthread_should_stop())
 			break;
