7607717f40474ea2883363a07613323e19bcb91a
renderbroken <zkennedy87@gmail.com>
2014-07-18 10:44:40 -0700

    kernel/power/powersuspend: LCD screen on/off hooks (Yank555.lu) - add an alternative hook for screen on / off detection in the display pannel driver - sleeps ~0.1s sooner, wakes up ~1s later then autosleep - clean up source formatting a bit (Paul Reioux)
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	kernel/power/powersuspend.c

--- include/linux/powersuspend.h
+++ include/linux/powersuspend.h
@@ -1,7 +1,7 @@
 /* include/linux/powersuspend.h
  *
  * Copyright (C) 2007-2008 Google, Inc.
- * Copyright (C) 2013 Paul Reioux 
+ * Copyright (C) 2013 Paul Reioux
  *
  * Modified by Jean-Pierre Rasquin <yank555.lu@gmail.com>
  *
@@ -9,6 +9,8 @@
  *         but use a hook in autosleep instead.
  *
  *  v1.2 - make kernel / userspace mode switchable
+ *
+ *  v1.3 - add a hook in display panel driver as alternative kernel trigger
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -29,8 +31,9 @@
 #define POWER_SUSPEND_INACTIVE	0
 #define POWER_SUSPEND_ACTIVE	1
 
-#define POWER_SUSPEND_KERNEL	0
-#define POWER_SUSPEND_USERSPACE	1
+#define POWER_SUSPEND_AUTOSLEEP	0	// Use kernel autosleep as hook
+#define POWER_SUSPEND_USERSPACE	1	// Use fauxclock as trigger
+#define POWER_SUSPEND_PANEL	2	// Use display pannel state as hook
 
 struct power_suspend {
 	struct list_head link;
@@ -41,7 +44,8 @@ struct power_suspend {
 void register_power_suspend(struct power_suspend *handler);
 void unregister_power_suspend(struct power_suspend *handler);
 
-void set_power_suspend_state_hook(int new_state);
+void set_power_suspend_state_autosleep_hook(int new_state);
+void set_power_suspend_state_panel_hook(int new_state);
 
 #endif
 
