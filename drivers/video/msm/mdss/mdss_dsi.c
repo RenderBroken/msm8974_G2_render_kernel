e4d9b1781991eec79e6c9a4f35ecc703336c2683
Dorimanx <yuri@bynet.co.il>
2014-08-08 12:14:25 +0300

    PowerSuspend: V1.5 Remove HYBRID Mode. as it's stuck LG MDSS!!!
    Users report random screen stuck in BLANK state. after many tests.
    
    This seems to be the problem. when using KERNEL (autosleep.c) all ok
    
    Maybe there is other drivers that do the bug. but i need stable kernel.
    
    So i can always switch to PANEL to test, but i dont think i will.
    
    Also USER/APP cant switch the kernel mode.
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -22,7 +22,9 @@
 #include <linux/err.h>
 #include <linux/regulator/consumer.h>
 #include <linux/lcd_notify.h>
-
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
 #ifdef CONFIG_MACH_LGE
 #include <mach/board_lge.h>
 #endif
@@ -1044,6 +1046,9 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 	switch (event) {
 	case MDSS_EVENT_UNBLANK:
 		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
 		rc = mdss_dsi_on(pdata);
 		mdss_dsi_op_mode_config(pdata->panel_info.mipi.mode,
 							pdata);
@@ -1067,6 +1072,9 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc = mdss_dsi_blank(pdata);
 		rc = mdss_dsi_off(pdata);
 		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		ctrl_pdata->ctrl_state &= ~CTRL_STATE_MDP_ACTIVE;
