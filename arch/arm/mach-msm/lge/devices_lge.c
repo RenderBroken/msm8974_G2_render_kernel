3d1fdc265d4c139d98f01134afb45fa67a786f2c
renderbroken <zkennedy87@gmail.com>
2014-09-13 09:06:50 -0700

    Implement Kexec Hardboot Patch Thanks to @PatrikKT Signed-off-by: renderbroken <zkennedy87@gmail.com>

--- arch/arm/mach-msm/lge/devices_lge.c
+++ arch/arm/mach-msm/lge/devices_lge.c
@@ -36,9 +36,12 @@
 #include <linux/spmi.h>
 #endif
 
-
 /* in drivers/staging/android */
 #include "ram_console.h"
+
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <linux/memblock.h>
+#endif
 
 static int cn_arr_len = 3;
 /*                                                              */
@@ -226,6 +229,27 @@ void __init lge_add_persist_ram_devices(void)
 
 void __init lge_reserve(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page - just after ram_console,
+	// at the start of second memory bank
+	int ret;
+	phys_addr_t start;
+	struct membank* bank;
+
+	if (meminfo.nr_banks < 2) {
+		pr_err("%s: not enough membank\n", __func__);
+		return;
+	}
+
+	bank = &meminfo.bank[1];
+	start = bank->start + SZ_1M + LGE_PERSISTENT_RAM_SIZE;
+	ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
+
 #if defined(CONFIG_ANDROID_PERSISTENT_RAM)
 	lge_add_persist_ram_devices();
 #endif
@@ -633,9 +657,9 @@ hw_rev_type lge_get_board_revno(void)
 	return lge_bd_rev;
 }
 #if defined(CONFIG_LCD_KCAL)
-/*             
-                          
-                                
+/*
+
+
 */
 int g_kcal_r = 255;
 int g_kcal_g = 255;
