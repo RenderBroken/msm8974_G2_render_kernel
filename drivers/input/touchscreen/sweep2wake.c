9a915a2d7f70bffa6c617eeb901799db2e98a43a
savoca <adeddo27@gmail.com>
2014-09-13 12:10:16 -0400

    sweep2wake: Adapt for TricksterMOD and disable s2w

--- drivers/input/touchscreen/sweep2wake.c
+++ drivers/input/touchscreen/sweep2wake.c
@@ -48,7 +48,6 @@ MODULE_LICENSE("GPLv2");
 /* Tuneables */
 #define S2W_DEBUG		0
 #define S2W_DEFAULT		0
-#define S2W_S2SONLY_DEFAULT	0
 #define S2W_PWRKEY_DUR          60
 
 /* Hammerhead aka Nexus 5 */
--- include/linux/input/sweep2wake.h
+++ include/linux/input/sweep2wake.h
@@ -21,6 +21,6 @@
 #ifndef _LINUX_SWEEP2WAKE_H
 #define _LINUX_SWEEP2WAKE_H
 
-extern int s2w_switch, s2w_s2sonly;
+extern int s2w_switch;
 
 #endif	/* _LINUX_SWEEP2WAKE_H */
