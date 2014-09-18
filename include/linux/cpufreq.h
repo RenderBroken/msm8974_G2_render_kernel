2e46bd1000921e90b547d5f7b30b1e0c97105ea8
Pranav Vashi <neobuddy89@gmail.com>
2014-09-18 21:15:47 +0530

    cpufreq: Add "bacon" governor
    * This is nothing but polished interactive governor
      branded as "bacon" since it was adapted from bacon
      device.
    * It is minimally tuned in the code. Full configuration
      is required in ramdisk for it to work properly.
    * Added on mass demand.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -388,6 +388,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BACON)
+extern struct cpufreq_governor cpufreq_gov_bacon;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bacon)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intellimm)
