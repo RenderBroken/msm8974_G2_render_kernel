155d9314687e69959f11ac26b2b6f4c320903c9a
Paul Reioux <reioux@gmail.com>
2014-11-20 11:24:57 -0600

    cpufreq: intelliminmax cpu governor baseline commit
    intelliminmax (intellimm) governor is designed to work
    with the newer SOCs with fixed voltage rails (ie MSM8974+ SOCs).
    It is designed to work within those fixed voltage ranges in
    order to maximize battery performance while creating a smooth UI
    operations.
    
    [neo: Adjust get_cpu_idle_time define to match our gov structure.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -417,6 +417,9 @@ extern struct cpufreq_governor cpufreq_gov_optimax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
+extern struct cpufreq_governor cpufreq_gov_intellimm;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intellimm)
 #endif
 
 
