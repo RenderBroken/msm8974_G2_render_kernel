011c7d8dbd80e2ae199a1a65fb63ffb1c70f23f4
Pranav Vashi <neobuddy89@gmail.com>
2014-11-23 09:38:40 +0530

    cpufreq: Add interface to set/get min/max freq
    * Interface similar to that for setting governor.
    * Interface can be used by drivers to change/get
      min/max freq per cpu.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -319,6 +319,10 @@ __ATTR(_name, 0644, show_##_name, store_##_name)
  *********************************************************************/
 int cpufreq_get_policy(struct cpufreq_policy *policy, unsigned int cpu);
 int cpufreq_update_policy(unsigned int cpu);
+int cpufreq_set_freq(unsigned int max_freq, unsigned int min_freq,
+			unsigned int cpu);
+int cpufreq_get_max(unsigned int cpu);
+int cpufreq_get_min(unsigned int cpu);
 
 #ifdef CONFIG_CPU_FREQ
 /* query the current CPU frequency (in kHz). If zero, cpufreq couldn't detect it */
