c5fa09698f9e1c1f29240c95f68fa4dbf99d3519
Pranav Vashi <neobuddy89@gmail.com>
2014-11-23 11:08:26 +0530

    cpufreq: Make get governor interface always return good value
    * In rare scenario, we may not able to lock policy
      and in such case, get governor interface returns
      "invalid" governor.
    * This patch ensures we never get "invalid" governor.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1876,14 +1876,14 @@ int cpufreq_set_freq(unsigned int max_freq, unsigned int min_freq,
 	} else {
 		cpu_policy = __cpufreq_cpu_get(cpu, 1);
 		if (!cpu_policy) {
-			put_online_cpus();
-			return -EINVAL;
+			val = per_cpu(cpufreq_policy_save, cpu).gov;
+			goto invalid;
 		}
 
 		if (lock_policy_rwsem_write(cpu) < 0) {
 			__cpufreq_cpu_put(cpu_policy, true);
-			put_online_cpus();
-			return -EINVAL;
+			val = per_cpu(cpufreq_policy_save, cpu).gov;
+			goto invalid;
 		}
 
 		if (max_freq && max_freq >= cpu_policy->min) {
@@ -1899,6 +1899,7 @@ int cpufreq_set_freq(unsigned int max_freq, unsigned int min_freq,
 
 		__cpufreq_cpu_put(cpu_policy, true);
 	}
+invalid:
 	put_online_cpus();
 
 	return ret;
