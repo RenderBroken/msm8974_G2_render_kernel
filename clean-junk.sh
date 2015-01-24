fce691a2fa73279f76f56e6dc4e4dcd70b8132fa
Zachariah <zkennedy87@gmail.com>
2015-01-23 20:33:39 -0500

    Clean-junk.sh: Add ability to remove *.patch
    Signed-off-by: Zachariah <zkennedy87@gmail.com>

--- clean-junk.sh
+++ clean-junk.sh
@@ -4,6 +4,8 @@ for i in `find . -type f \( -iname \*.rej \
                                 -o -iname \*.orig \
                                 -o -iname \*.bkp \
                                 -o -iname \*.ko \
+                                -o -iname \*.patch \
+                                -o -iname \*.patch.[0-9]* \
                                 -o -iname \*.c.BACKUP.[0-9]*.c \
                                 -o -iname \*.c.BASE.[0-9]*.c \
                                 -o -iname \*.c.LOCAL.[0-9]*.c \
