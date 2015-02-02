02b520012c49a8c59680faa6a634b031198ffb60
Zachariah Kennedy <zkennedy87@gmail.com>
2015-02-01 22:09:13 -0500

    cpufreq: interactive: rework interactive
    No more Bacon Governor. Now Interactive is merged with Bacon. Also cleanup governors. Thanks @neobuddy89!
    
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -391,9 +391,6 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intellimm)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BACON)
-extern struct cpufreq_governor cpufreq_gov_bacon;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bacon)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
 extern struct cpufreq_governor cpufreq_gov_HYPER;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_HYPER)
